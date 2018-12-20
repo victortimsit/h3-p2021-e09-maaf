4ee4998b4ea0201acf303d63a0ab06d6d217b091
edhbr <edouard.haberer@gmail.com>
2018-12-20 15:43:32 +0100

    :wrench: remove user scalable

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,11 @@ module.exports = {
     },
     meta: [
       { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        name: "viewport",
+        content:
+          "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+      },
       { hid: "description", name: "description", content: pkg.description },
       { name: "msapplication-TileColor", content: "#2b5797" },
       { name: "theme-color", content: "#ffffff" }
